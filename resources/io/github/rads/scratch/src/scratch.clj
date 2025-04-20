d27ad5df870596ece67b644e9f1f8cf2b002548b
Radford Smith <11401+rads@users.noreply.github.com>
2025-04-20 10:32:49 -0700

    Only include `-main` function

--- resources/io/github/rads/scratch/src/scratch.clj
+++ resources/io/github/rads/scratch/src/scratch.clj
@@ -1,12 +1,5 @@
 (ns {{scratch/ns}}
   "{{description}}")
 
-(defn exec
-  "Invoke me with clojure -X {{scratch/ns}}/exec"
-  [opts]
-  (println "exec with" opts))
-
-(defn -main
-  "Invoke me with clojure -M -m {{scratch/ns}}"
-  [& args]
-  (println "-main with" args))
+(defn -main [& args]
+  (println args))
